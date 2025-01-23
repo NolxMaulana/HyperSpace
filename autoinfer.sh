9c1facde8a63a257211ae2a585a7b53f8dfc3314
NolxMaulana <nolxmaulana.nft@ud.me>
2025-01-23 12:28:09 +0700

    Update autoinfer.sh

--- autoinfer.sh
+++ autoinfer.sh
@@ -1,5 +1,5 @@
 echo "Menjalankan inferensi menggunakan model yang telah ditambahkan..."
-    do
+   while true: do
         if aios-cli infer --model hf:afrideva/Tiny-Vicuna-1B-GGUF:tiny-vicuna-1b.q4_k_m.gguf --prompt "how do I support the Share it hub community?"; then
             echo "Inferensi berhasil."
             break
