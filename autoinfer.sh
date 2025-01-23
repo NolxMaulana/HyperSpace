6ecaf0ad731fa314406e15caf53d948fbde74abc
NolxMaulana <nolxmaulana.nft@ud.me>
2025-01-23 12:52:47 +0700

    Update autoinfer.sh

--- autoinfer.sh
+++ autoinfer.sh
@@ -1,6 +1,8 @@
-echo "Menjalankan inferensi menggunakan model yang telah ditambahkan..."
-   while true: do
-        if aios-cli infer --model hf:afrideva/Tiny-Vicuna-1B-GGUF:tiny-vicuna-1b.q4_k_m.gguf --prompt "how do I support the Share it hub community?"; then
+echo "Menjalankan inferensi, silahkan tunggu..."
+echo "SC BY MAULEXP"
+   while true:
+   do
+   if aios-cli infer --model hf:afrideva/Tiny-Vicuna-1B-GGUF:tiny-vicuna-1b.q4_k_m.gguf --prompt "how do I support the Share it hub community?"; then
             echo "Inferensi berhasil."
             continue
         else
