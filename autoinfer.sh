76bcac445adebb44aff7bb001575df85d2ce2e68
NolxMaulana <nolxmaulana.nft@ud.me>
2025-01-23 12:25:24 +0700

    Update autoinfer.sh

--- autoinfer.sh
+++ autoinfer.sh
@@ -1,7 +1,4 @@
 echo "Menjalankan inferensi menggunakan model yang telah ditambahkan..."
-read -p "Apakah Anda ingin menjalankan inferensi? (y/n): " user_choice
-
-if [[ "$user_choice" == "y" || "$user_choice" == "Y" ]]; then
     do
         if aios-cli infer --model hf:afrideva/Tiny-Vicuna-1B-GGUF:tiny-vicuna-1b.q4_k_m.gguf --prompt "how do I support the Share it hub community?"; then
             echo "Inferensi berhasil."
