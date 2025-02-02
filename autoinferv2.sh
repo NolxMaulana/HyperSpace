89db8a06638360d1ec8fd2c8ff36f34afa21d349
NolxMaulana <nolxmaulana.nft@ud.me>
2025-02-03 01:08:49 +0700

    Create autoinferv2.sh

---
+++ autoinferv2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Membaca file chat.txt
+addressList=$(cat chat.txt)
+
+# Mengubah isi file menjadi array
+addressListArray=($(echo "$addressList" | tr '\n' ' '))
+
+# Melakukan perulangan dari index 0
+for ((index=0; index<${#addressListArray[@]}; index++)); do
+  Wallet=${addressListArray[$index]}
+  echo "Content Chat: $Wallet"
+
+  # Menggunakan curl untuk mengirimkan POST request
+  response=$(curl -X POST \
+    https://api.hive.aios.network/v1/inference \
+    -H 'accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -d '{"messages": [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": "'"$Wallet"'" }]}')
+
+  # Menampilkan hasil response
+  echo "Response: $(echo "$response" | jq -r '.choices[0].message.content')"
+  echo "WAIT 5 DETIK"
+
+  # Menunggu 5 detik
+  sleep 5
+done
