2fb07927ae2390ead8c94e82c0cde4df26f12569
NolxMaulana <nolxmaulana.nft@ud.me>
2025-02-03 03:51:44 +0700

    Update autoinferv2.sh

--- autoinferv2.sh
+++ autoinferv2.sh
@@ -1,25 +1,10 @@
+echo "SC BY MAULEXP"
+model="hf:afrideva/Tiny-Vicuna-1B-GGUF:tiny-vicuna-1b.q4_k_m.gguf"
 # Membaca file chat.txt
-addressList=$(cat chat.txt)
-
-# Mengubah isi file menjadi array
-addressListArray=($(echo "$addressList" | tr '\n' ' '))
-
-# Melakukan perulangan dari index 0
-for ((index=0; index<${#addressListArray[@]}; index++)); do
-  Wallet=${addressListArray[$index]}
-  echo "Content Chat: $Wallet"
-
-  # Menggunakan curl untuk mengirimkan POST request
-  response=$(curl -X POST \
-    https://api.hive.aios.network/v1/inference \
-    -H 'accept: application/json' \
-    -H 'Content-Type: application/json' \
-    -d '{"messages": [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": "'"$Wallet"'" }]}')
-
-  # Menampilkan hasil response
-  echo "Response: $(echo "$response" | jq -r '.choices[0].message.content')"
-  echo "WAIT 5 DETIK"
-
-  # Menunggu 5 detik
-  sleep 5
-done
+while read -r line; do
+  # Menjalankan perintah aios-cli infer untuk setiap argument
+  if aios-cli infer --model $model --prompt "$line";
+   then
+    echo "Inferensi berhasil."
+  fi
+done < chat.txt
